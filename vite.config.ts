40196242d46b6a49595207f6badc8d9988024a1e
ayoung-iya <rhaehfdl1879@naver.com>
2024-03-15 00:51:36 +0900

    Chore: svg파일을 react component로 사용하기 위해 vite-plugin-svgr 설치

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 // https://vitejs.dev/config/
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
+import svgr from "vite-plugin-svgr";
 
 export default defineConfig(({ command, mode }) => {
   const base = {
-    plugins: [react()],
+    plugins: [svgr(), react()],
   };
 
   /**
